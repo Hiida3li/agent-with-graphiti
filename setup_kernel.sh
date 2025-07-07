cb82d70c15f111635f4e7ba09628b869423c8574
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2025-07-07 10:35:05 +0400

    .

--- setup_kernel.sh
+++ setup_kernel.sh
@@ -3,3 +3,5 @@ ENV_NAME=${1:-default-env}
 DISPLAY_NAME=${2:-"Python ($ENV_NAME)"}
 
 python -m ipykernel install --user --name=$ENV_NAME --display-name "$DISPLAY_NAME"
+
+bash setup_kernel.sh orki-env "Python (orki-env)"
