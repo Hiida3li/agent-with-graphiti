a6e0bb016aaa6f95bcd1584b9550aaa1c33c7c84
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2025-07-13 15:29:01 +0400

    llm provider on parallel mode

--- setup.py
+++ setup.py
@@ -1,23 +1,59 @@
+# import asyncio
+# import google.generativeai as genai
+# from dotenv import load_dotenv
+# import os
+# load_dotenv()
+#
+# GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+#
+# async def main():
+#     client = genai.Client()
+#
+#     print("Generating content asynchronously...")
+#
+#     response = await client.aio.models.generate_content(
+#         model="gemini-2.5-flash",
+#         contents="Write a short story about a robot and another stroy about clouds."
+#     )
+#
+#     print(response.text)
+#
+# if __name__ == "__main__":
+#     asyncio.run(main())
+
 import asyncio
-import google.generativeai as genai
+from google import genai
 from dotenv import load_dotenv
 import os
-load_dotenv()
 
+load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
-async def main():
+
+async def generate_content_task():
     client = genai.Client()
 
     print("Generating content asynchronously...")
 
     response = await client.aio.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Write a short story about a robot and another stroy about clouds."
+        contents="Write a short story about a robot"
     )
 
-    print(response.text)
+    print("\n Gemini Output:\n", response.text)
+
+
+async def do_something_else():
+    for i in range(3):
+        print(f"💡 Doing something else... step {i+1}")
+        await asyncio.sleep(1)
+
+async def main():
+    await asyncio.gather(
+        generate_content_task(),
+        do_something_else()
+    )
+
 
 if __name__ == "__main__":
     asyncio.run(main())
-
